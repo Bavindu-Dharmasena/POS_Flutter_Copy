98d0f5f7ca83c73d038f89c28a047fc6346f7bfc
mihiranwijesekara <achinthamihiran654@gmail.com>
2025-08-14 10:54:55 +0530

    create compnonet stock keeper report card

--- lib/features/stockkeeper/stockkeeper_reports.dart
+++ lib/features/stockkeeper/stockkeeper_reports.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:pos_system/widget/stock_Keeper_Report/category_filter_bar.dart';
+import 'package:pos_system/widget/stock_Keeper_Report/ModernCategoryFilterBar.dart';
 import 'package:pos_system/widget/stock_Keeper_Report/ModernReportCard.dart';
 import 'package:pos_system/widget/stock_Keeper_Report/report_preview_dialog.dart';
 import 'package:pos_system/widget/stock_Keeper_Report/ModernSectionHeader.dart';
@@ -1388,109 +1388,4 @@ class _StockKeeperReportsState extends State<StockKeeperReports>
       ),
     );
   }
-}
-
-// Modern Category Filter Bar Widget
-class ModernCategoryFilterBar extends StatelessWidget
-    implements PreferredSizeWidget {
-  final String selectedCategory;
-  final List<String> categories;
-  final IconData Function(String) getCategoryIcon;
-  final bool isMobile;
-  final Function(String) onCategorySelected;
-
-  const ModernCategoryFilterBar({
-    Key? key,
-    required this.selectedCategory,
-    required this.categories,
-    required this.getCategoryIcon,
-    required this.isMobile,
-    required this.onCategorySelected,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      margin: const EdgeInsets.only(bottom: 8),
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        itemCount: categories.length,
-        itemBuilder: (context, index) {
-          final category = categories[index];
-          final isSelected = category == selectedCategory;
-
-          return Padding(
-            padding: const EdgeInsets.only(right: 12),
-            child: RawKeyboardListener(
-              focusNode: FocusNode(),
-              onKey: (RawKeyEvent event) {
-                if (event is RawKeyDownEvent &&
-                    event.logicalKey == LogicalKeyboardKey.enter) {
-                  onCategorySelected(category);
-                }
-              },
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  onTap: () => onCategorySelected(category),
-                  borderRadius: BorderRadius.circular(12),
-                  child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 300),
-                    padding: EdgeInsets.symmetric(
-                      horizontal: isMobile ? 12 : 16,
-                      vertical: 8,
-                    ),
-                    decoration: BoxDecoration(
-                      gradient: isSelected
-                          ? const LinearGradient(
-                              colors: [Color(0xFF64FFDA), Color(0xFF1DE9B6)],
-                            )
-                          : null,
-                      color: isSelected ? null : Colors.white.withOpacity(0.1),
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(
-                        color: isSelected
-                            ? Colors.transparent
-                            : Colors.white.withOpacity(0.2),
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          getCategoryIcon(category),
-                          size: isMobile ? 16 : 18,
-                          color: isSelected
-                              ? const Color(0xFF0F0F23)
-                              : Colors.white.withOpacity(0.8),
-                        ),
-                        const SizedBox(width: 8),
-                        Text(
-                          category,
-                          style: TextStyle(
-                            color: isSelected
-                                ? const Color(0xFF0F0F23)
-                                : Colors.white.withOpacity(0.8),
-                            fontWeight: isSelected
-                                ? FontWeight.bold
-                                : FontWeight.w500,
-                            fontSize: isMobile ? 12 : 14,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
-  }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(50);
 }
